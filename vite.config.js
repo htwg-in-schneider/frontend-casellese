f9b502a2594c9f8f7889772c152e5bc492871569
nucccio <ca591sav@htwg-konstanz.de>
2025-11-26 15:20:48 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const repoName = '/frontend-casellese'; 
+const repoName = 'frontend-casellese'; 
 
 // https://vite.dev/config/
 export default defineConfig({
